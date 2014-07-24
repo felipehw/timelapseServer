84ec947dcc88b3a7c3c26b85ce501aa32093958d
Felipe Wegner <felipehw@gmail.com>
2014-07-24 18:09:33 -0300

    Implementado servidor de arquivos estáticos

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ server.route(getTimelapses);
 server.route(getTimelapsesTimelapse);
 server.route(getTimelapsesTimelapsePhotos);
 
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: 'public',
+      //listing: true,
+      index: true,
+      lookupCompressed: true,
+      defaultExtension: 'html',
+    }
+  }
+});
+
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
